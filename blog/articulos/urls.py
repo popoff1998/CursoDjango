229ef01e9e0c4bfbfa0daf26b811d940ac0fcae3
tonin <tonin@uco.es>
2019-10-26 22:09:50 +0200

    faltan cosas

--- blog/articulos/urls.py
+++ blog/articulos/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('detalle/<slug:slug>', views.DetailView.as_view(), name='detalle')
+    path('detalle/<slug:slug>/', views.DetailView.as_view(), name='detalle'),
+    path('autores/', views.AutoresView.as_view(), name='autores'),
+    path('autor/<str:username>/', views.DetailAutorView.as_view(), name='autor'),
 ]
